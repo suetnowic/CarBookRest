c874e57472b5c0c1eee931a8f8e4f04dd1a9f261
Viktor <suetnovvic@yandex.com>
2022-03-28 21:22:54 +0300

    created binding result for validation errors

---
+++ src/main/java/com/viktorsuetnov/carbook/web/AuthController.java
@@ -0,0 +1,14 @@
+package com.viktorsuetnov.carbook.web;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+@PreAuthorize("permitAll()")
+public class AuthController {
+
+}

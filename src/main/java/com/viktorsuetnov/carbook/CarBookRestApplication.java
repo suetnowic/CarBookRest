31465f50a2f44c89a5b0b187f531a41be2deb051
Viktor <suetnovvic@yandex.com>
2022-03-28 14:46:16 +0300

    updated user model and created user repository

---
+++ src/main/java/com/viktorsuetnov/carbook/CarBookRestApplication.java
@@ -0,0 +1,13 @@
+package com.viktorsuetnov.carbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBookRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBookRestApplication.class, args);
+    }
+
+}
--- src/main/java/com/viktorsuetnov/carbook/entity/User.java
+++ src/main/java/com/viktorsuetnov/carbook/entity/User.java
@@ -1,19 +1,38 @@
 package com.viktorsuetnov.carbook.entity;
 
 import com.viktorsuetnov.carbook.entity.enums.ERole;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
 import java.util.*;
 
+@Data
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(length = 120)
     private String password;
 
-    private Set<ERole> roles = new HashSet<ERole>();
-    private List<Car> cars = new ArrayList<Car>();
+    @ElementCollection(targetClass = ERole.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<ERole> roles = new HashSet<>();
 
+//    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "owner", orphanRemoval = true)
+//    private List<Car> cars = new ArrayList<Car>();
+
+    @Transient
     private Collection<? extends GrantedAuthority> authorities;
 }

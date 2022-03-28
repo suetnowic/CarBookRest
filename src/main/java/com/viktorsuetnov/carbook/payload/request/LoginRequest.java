50d0fa1144b176b774a6ded138ba2617869ef318
Viktor <suetnovvic@yandex.com>
2022-03-28 20:27:46 +0300

    created validator email

---
+++ src/main/java/com/viktorsuetnov/carbook/payload/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.viktorsuetnov.carbook.payload.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class LoginRequest {
+
+    @NotEmpty(message = "username cannot be empty")
+    private String username;
+    @NotEmpty(message = "password cannot be empty")
+    private String password;
+
+}
